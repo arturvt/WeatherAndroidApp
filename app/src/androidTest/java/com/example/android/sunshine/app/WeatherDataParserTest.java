9c35125320abdc2f79514c44bfd49e6a474f686d
Artur Tenorio <atenorio@motorola.com>
2016-12-08 20:03:38 -0300

    Several changes to make the app download content from server

---
+++ app/src/androidTest/java/com/example/android/sunshine/app/WeatherDataParserTest.java
@@ -0,0 +1,10 @@
+package com.example.android.sunshine.app;
+
+/**
+ * Created by atenorio on 12/6/16.
+ */
+
+public class WeatherDataParserTest {
+
+
+}
---
+++ app/src/main/java/com/example/android/sunshine/app/VolleyCallBack.java
@@ -0,0 +1,9 @@
+package com.example.android.sunshine.app;
+
+/**
+ * Created by atenorio on 12/6/16.
+ */
+
+public interface VolleyCallBack {
+    void onSuccessResponse(String[] result);
+}
