827dd9a82938626a412c733126007be688dc1dbb
artur <arturvt@gmail.com>
2016-12-27 11:37:10 -0300

    Adding shared info. Adding instrumentation tests.
    TODO: Not working properly the share

--- app/src/androidTest/java/com/example/android/sunshine/app/WeatherDataParserTest.java
+++ app/src/androidTest/java/com/example/android/sunshine/app/WeatherDataParserTest.java
@@ -1,10 +1,19 @@
 package com.example.android.sunshine.app;
 
+import android.support.test.filters.SmallTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
 /**
  * Created by atenorio on 12/6/16.
  */
 
+@RunWith(AndroidJUnit4.class)
+@SmallTest
 public class WeatherDataParserTest {
+    public static final String TEST_STRING = "This is a string";
+    public static final long TEST_LONG = 12345678L;
 
-
+    // Check this: https://developer.android.com/training/testing/unit-testing/instrumented-unit-tests.html#build
 }
